33aaae2698ca63a19b3081eac39079438bec8489
Mohammad Ahmad Khan <mohdahmad6300@gmail.com>
2026-09-25 23:15:21 +0530

    Updated Student Mangement System JDBC Project.

--- src/com/jdbc/JDBCExample.java
+++ src/com/jdbc/JDBCExample.java
@@ -1,11 +1,14 @@
 package com.jdbc;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class JDBCExample {
     static  String URL = "jdbc:mysql://localhost:3306/school";
     static String USER = "root";
     static String PASSWORD = "ahmad@632";
+
+    static Scanner sc = new Scanner(System.in);
 
     public void connectDatabase() {
 
@@ -24,7 +27,7 @@ public class JDBCExample {
         }
     }
 
-    public void readData()
+    public void viewAllData()
     {
         String query = "select * from students";
 
@@ -34,6 +37,9 @@ public class JDBCExample {
                 ResultSet rs = stmt.executeQuery(query);
         )
         {
+            System.out.println("===================================================================");
+            System.out.println("==========================[ ALL STUDENTS ]=========================");
+            System.out.println("===================================================================");
 
             while (rs.next()) {
                 int id = rs.getInt("id");
@@ -41,10 +47,10 @@ public class JDBCExample {
                 int age = rs.getInt("age");
                 String course = rs.getString("course");
 
-                System.out.println(
-                        id + " " + name + " " + age + " " + course
-                );
+                System.out.println("[ ID: " + id+ " ] [ Name: " + name + " ] [ Age: " + age + " ] [ Course: " + course+" ]");
             }
+            System.out.println("===================================================================");
+            System.out.println("===================================================================");
         }
 
         catch (SQLException e)
@@ -62,14 +68,33 @@ public class JDBCExample {
                 PreparedStatement ps = conn.prepareStatement(query);
                 )
         {
-            ps.setInt(1,7);
-            ps.setString(2,"Aboozar");
-            ps.setInt(3,11);
-            ps.setString(4,"C++");
+            System.out.print("Enter Student ID : ");
+            int id = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Enter Student Name : ");
+            String name = sc.nextLine();
+
+            System.out.print("Enter Student Age : ");
+            int age = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Enter Student Course : ");
+            String course = sc.nextLine();
+
+
+            ps.setInt(1,id);
+            ps.setString(2,name);
+            ps.setInt(3,age);
+            ps.setString(4,course);
 
 
             int rows = ps.executeUpdate();
-            System.out.println("Inserted " + rows + " rows into students table");
+            if(rows > 0)
+                {
+                    System.out.println("Inserted " + rows + " rows into students table");
+                }
+
         }
         catch (SQLException e)
         {
@@ -86,13 +111,36 @@ public class JDBCExample {
                 PreparedStatement ps = conn.prepareStatement(query);
                 )
         {
-                ps.setString(1,"Neha");
-                ps.setInt(2,28);
-                ps.setString(3,"Advance JAVA");
-                ps.setInt(4,8);
+
+            System.out.print("Enter Student ID to update : ");
+            int id = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Set Student Name : ");
+            String name = sc.nextLine();
+
+            System.out.print("Set Student Age : ");
+            int age = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Set Student Course : ");
+            String course = sc.nextLine();
+
+                ps.setString(1,name);
+                ps.setInt(2,age);
+                ps.setString(3,course);
+                ps.setInt(4,id);
 
                 int rows = ps.executeUpdate();
-            System.out.println("Updated " + rows + " rows into students table");
+
+                if(rows > 0)
+                    {
+                        System.out.println("Updated " + rows + " rows into students table");
+                    }
+                else
+                    {
+                        System.out.println("Student with ID " + id + " does not exist");
+                    }
         }
 
         catch (SQLException e)
@@ -110,10 +158,57 @@ public class JDBCExample {
                 PreparedStatement ps = conn.prepareStatement(query);
                 )
         {
-            ps.setInt(1,7);
+            System.out.print("Enter Student ID to delete Student's Data : ");
+            int id = sc.nextInt();
+
+            ps.setInt(1,id);
 
             int rows = ps.executeUpdate();
-            System.out.println("Deleted " + rows + " rows into students table");
+            if(rows > 0)
+                System.out.println("Deleted " + rows + " rows into students table");
+            else
+                System.out.println("Student with ID " + id + " does not exist");
+
+        }
+
+        catch (SQLException e)
+        {
+            System.out.println("Error connecting to database : " + e);
+        }
+    }
+
+    public void searchData()
+    {
+        String query = "select * from students where id = ?";
+
+        try(
+                Connection conn = DriverManager.getConnection(URL,USER,PASSWORD);
+                PreparedStatement ps = conn.prepareStatement(query);
+
+                )
+        {
+            System.out.print("Enter Student ID to search : ");
+            int id = sc.nextInt();
+
+            ps.setInt(1,id);
+
+           try(ResultSet rs = ps.executeQuery();) {
+               if (rs.next()) {
+                   System.out.println("============================================");
+                   System.out.println("==============[STUDENT FOUND]===============");
+                   System.out.println("============================================");
+
+                   System.out.println("ID     : " + rs.getInt("id"));
+                   System.out.println("Name   : " + rs.getString("name"));
+                   System.out.println("Age    : " + rs.getInt("age"));
+                   System.out.println("Course : " + rs.getString("course"));
+
+                   System.out.println("============================================");
+                   System.out.println("============================================");
+               } else {
+                   System.out.println("Student with ID " + id + " does not exist");
+               }
+           }
         }
 
         catch (SQLException e)
--- src/com/jdbc/RunJDBC.java
+++ src/com/jdbc/RunJDBC.java
@@ -1,14 +1,55 @@
 import com.jdbc.JDBCExample;
 
-class RunJDBC
-{
-    public static void main(String[] args)
-    {
-        JDBCExample jdbc = new JDBCExample();
-        jdbc.connectDatabase();
-        jdbc.readData();
-        jdbc.addData();
-        jdbc.updateData();
-        jdbc.deleteData();
+import java.util.Scanner;
+
+class RunJDBC {
+
+
+    public static void main(String[] args) {
+        while (true) {
+            JDBCExample jdbc = new JDBCExample();
+            System.out.println("\n==============================");
+            System.out.println(" STUDENT MANAGEMENT SYSTEM");
+            System.out.println("==============================");
+
+            System.out.println("1. Add Student");
+            System.out.println("2. View All Students");
+            System.out.println("3. Update Student");
+            System.out.println("4. Delete Student");
+            System.out.println("5. Search Student");
+            System.out.println("6. Exit");
+
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Enter your choice : ");
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    jdbc.addData();
+                    break;
+
+                case 2:
+                    jdbc.viewAllData();
+                    break;
+
+                case 3:
+                    jdbc.updateData();
+                    break;
+
+                case 4:
+                    jdbc.deleteData();
+                    break;
+                case 5:
+                    jdbc.searchData();
+                    break;
+                case 6:
+                    System.out.println("Thanks for using my application ");
+                    System.exit(0);
+                    return;
+                default:
+                    System.out.println("Invalid choice ! ");
+            }
+        }
+
     }
 }
